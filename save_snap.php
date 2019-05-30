e5b64e84bd8280980107c50af700219f69d03434
ARNAB MUNSHI <arnab47stars@gmail.com>
2019-05-30 16:53:07 +0530

    Update save_snap.php

--- save_snap.php
+++ save_snap.php
@@ -9,7 +9,15 @@ $dbcon = mysqli_connect($dbhost,$dbusername,$dbpassword,$dbname) or die("Could n
 
 $data_uri = $_POST['data_uri'];
 
-$sql = "INSERT INTO capture_snap (image) VALUES ('{$data_uri}')";
+$image = file_get_contents($data_uri);
+$image_name = time().'.png';
+$fp  = fopen(__DIR__."./images/".$image_name, 'w+');
+fputs($fp, $image);
+fclose($fp);
+unset($image);
+
+
+$sql = "INSERT INTO capture_snap (image) VALUES ('{$image_name}')";
 
 if (mysqli_query($dbcon, $sql)) {
   echo 'Saved';
